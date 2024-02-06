f85a9a44b64ccc956c7b8d2e9bb151f1fa5e707c
minajchy <minajchy.123@gmail.com>
2024-02-06 12:58:17 +0530

    Expense Item

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 30.12,
+      date: new Date(2020, 7, 14),
+      locationOfExpenditure: "Algapur",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 18799.49,
+      date: new Date(2021, 2, 12),
+      locationOfExpenditure: "Hailakandi",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 2294.67,
+      date: new Date(2021, 2, 28),
+      locationOfExpenditure: "Silchar",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 15450,
+      date: new Date(2021, 5, 12),
+      locationOfExpenditure: "Algapur",
+    },
+    {
+      id: "e5",
+      title: "Mobile",
+      amount: 25599,
+      date: new Date(2021, 10, 18),
+      locationOfExpenditure: "Hailakandi",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div><h2>Expense Item!</h2></div>
+      {expenses.map((expence) => (
+        <ExpenseItem
+          key={expence.id}
+          title={expence.title}
+          date={expence.date}
+          amount={expence.amount}
+          locationOfExpenditure={expence.locationOfExpenditure}
+        ></ExpenseItem>
+      ))}
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2 className="expense-item h2">{props.title}</h2>
+        <div className="expense-item__price">Rs {props.amount}</div>
+        <div className="expense-item__location">
+          {props.locationOfExpenditure}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
